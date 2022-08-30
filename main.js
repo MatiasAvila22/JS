5574a1902b4bc1fd159c53c1be0a84e64f459c35
MatiasAvila22 <108633736+MatiasAvila22@users.noreply.github.com>
2022-08-29 21:08:19 -0300

    Simulador interactivo

---
+++ main.js
@@ -0,0 +1,95 @@
+let nombreUsuario;
+let producto1 = "PAPAS x 500gr";
+let producto2 = "HAMBURGUESAS x24";
+let producto3 = "PATITAS DE POLLO x kg" ;
+let producto4 = "CARITAS x 250gr";
+let precio1 = 500;
+let precio2 = 2500;
+let precio3 = 700;
+let precio4 = 400;
+
+do {
+    nombreUsuario = prompt("Ingrese su nombre por favor");
+} while (nombreUsuario === "" || isNaN(nombreUsuario) === false );
+alert("Hola "+nombreUsuario);
+
+const Productos = () =>{
+    let seleccion = parseInt(prompt("Elegi un producto: \n1)"+producto1+" con un precio de $"+precio1+"\n2)"+producto2+" con un precio de $"+precio2+"\n3)"+producto3+" con un precio de $"+precio3+"\n4)"+producto4+" con un precio de $"+precio4));
+    return seleccion;
+} 
+function Pago(){
+    let formaPago;
+    while(formaPago!=1 && formaPago!=2 && formaPago!=3 && formaPago!=4){
+    formaPago = parseInt(prompt("De que forma desea pagar?\n1) Efectivo \n2) Débito \n3) 3 cuotas sin interés\n4) Transferencia"));
+    }
+    return formaPago;
+}
+let productoSeleccionado = Productos();
+let numeroAleatorio = (Math.round(Math.random()*10));
+let seleccionPago = Pago();
+const CalculoPago = (producto) =>{
+    switch (producto) {
+        case 1:
+            switch (seleccionPago) {
+                case 1:
+                    alert(nombreUsuario+" deberás pagar $"+(precio1));
+                    break;
+                case 2:
+                    alert(nombreUsuario+" deberás pagar $"+(precio1));
+                    break;
+                case 3:
+                    alert(nombreUsuario+" deberás pagar 3 cuotas de: $"+(precio1));
+                    break;
+                case 4:
+                    alert(nombreUsuario+" deberás transferir: $"+(precio1));
+                    break;
+            }
+        case 2:
+            switch (seleccionPago) {
+                case 1:
+                    alert(nombreUsuario+" deberás pagar $"+(precio2));
+                    break;
+                case 2:
+                    alert(nombreUsuario+" deberás pagar $"+(precio2));
+                    break;
+                case 3:
+                    alert(nombreUsuario+" deberás pagar 3 cuotas de: $"+(precio2));
+                    break;
+                case 4:
+                    alert(nombreUsuario+" deberás transferir: $"+(precio2));
+                    break;
+            }
+            case 3:
+                switch (seleccionPago) {
+                    case 1:
+                        alert(nombreUsuario+" deberás pagar $"+(precio3));
+                        break;
+                    case 2:
+                        alert(nombreUsuario+" deberás pagar $"+(precio3));
+                        break;
+                    case 3:
+                        alert(nombreUsuario+" deberás pagar 3 cuotas de: $"+(precio3));
+                        break;
+                    case 4:
+                        alert(nombreUsuario+" deberás transferir: $"+(precio3));
+                        break;
+                }
+                case 4:
+                    switch (seleccionPago) {
+                        case 1:
+                            alert(nombreUsuario+" deberás pagar $"+(precio4));
+                            break;
+                        case 2:
+                            alert(nombreUsuario+" deberás pagar $"+(precio4));
+                            break;
+                        case 3:
+                            alert(nombreUsuario+" deberás pagar 3 cuotas de: $"+(precio4));
+                            break;
+                        case 4:
+                            alert(nombreUsuario+" deberás transferir: $"+(precio4));
+                            break;
+                    }                
+    }
+}
+
+
